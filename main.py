cafaed53e75d4fc5a7da1a5e341ba4f737b543ae
Khalil <khalilfoulks@gmail.com>
2021-01-12 13:08:41 -0500

    created function to pair games and release dates

--- main.py
+++ main.py
@@ -27,8 +27,33 @@ release_dates = soup.find_all('div', class_='first-release-date')
 
 # print(titles)
 
-for game in games:
-    print(game.text)
+# for game in games:
+#     print(game.text)
 
-for date in release_dates:
-    print(date.text)
+# for date in release_dates:
+#     print(date.text)
+
+
+def game_release_dates():
+    cache = {}
+    game_titles = []
+    dates = []
+
+    for game in games:
+        game_titles.append(game.text)
+
+    for date in release_dates:
+        dates.append(date.text)
+
+    print(game_titles)
+    print(dates)
+    print('')
+
+    for idx_g, game in enumerate(game_titles):
+        for idx_d, date in enumerate(dates):
+            if idx_g == idx_d:
+                cache[game] = date 
+    
+    print(cache)
+
+game_release_dates()
