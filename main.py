66924393b57cf11b496d3fd0a864c377757626ed
Khalil <khalilfoulks@gmail.com>
2021-01-14 12:37:37 -0500

    added helper function

--- main.py
+++ main.py
@@ -29,6 +29,19 @@ game_platforms = soup.find_all('div', class_='platforms')
 def remove(string): 
     return string.replace(" ", "") 
 
+def cache_mapping(cache, game_titles, game_info):
+    for idx_g, game in enumerate(game_titles):
+        if game not in cache:
+            for idx_j, info in enumerate(game_info):
+                if idx_g == idx_j:
+                    cache[game] = [info]
+
+        else:
+            for idx_j, info in enumerate(game_info):
+                if idx_g == idx_j:
+                    old_val = cache[game]
+                    old_val.append(info)   
+
 def game_info():
     cache = {}
     game_titles = []
@@ -54,24 +67,19 @@ def game_info():
     # print(platforms)
     # print('')
 
-    for idx_g, game in enumerate(game_titles):
-        for idx_d, date in enumerate(dates):
-            if idx_g == idx_d:
-                cache[game] = [date]
-
-        for idx_s, score in enumerate(scores):
-            if idx_g == idx_s:
-                old_val = cache[game]
-                old_val.append(score)
-        
-        for idx_p, plat in enumerate(platforms):
-            if idx_g == idx_p:
-                old_val = cache[game]
-                old_val.append(plat)
+    cache_mapping(cache, game_titles, dates)
+    cache_mapping(cache, game_titles, scores)
+    cache_mapping(cache, game_titles, platforms)
     
     # print(cache)
-
+    
     for game in cache:
-        print(f'{game} released on {cache[game][0]}, 2020 for "{cache[game][2].strip()}" and recieved an avg score of {remove(cache[game][1])} \n')
+        print(f'''
+        Game Name: {game} 
+        Release Date: {cache[game][0]}, 2020
+        Platforms: {cache[game][2].strip()} 
+        Score: {remove(cache[game][1])} 
+        ''')     
+
 
 game_info()
