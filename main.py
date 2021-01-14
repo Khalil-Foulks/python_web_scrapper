ede31defbd9ee849fba743ddc52a7f6121891ba2
Khalil <khalilfoulks@gmail.com>
2021-01-14 16:57:43 -0500

    removed print statements

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 import time
-import os.path
 
 # uncomment code below if reaching recursion limit
 # import sys
@@ -16,12 +15,10 @@ def game_scrapper(file_mode, page_num=1):
     soup = BeautifulSoup(page, 'lxml')
 
     games_list = soup.find_all('div', class_='row no-gutters py-2 game-row align-items-center')
-    # print(games_list)
 
     # scrapper go through every page
     if len(games_list) > 0:
         for idx, games in enumerate(games_list):
-            # print(games)
             game_rank = games.find('div', class_='rank').text.strip()
             game_name = games.find('div', class_='game-name col').text
             release_dates = games.find('div', class_='first-release-date').text
@@ -29,7 +26,6 @@ def game_scrapper(file_mode, page_num=1):
             game_platforms = games.find('div', class_='platforms').text.strip()
             game_link = 'https://opencritic.com' + games.a['href']
 
-            # print(f'Game Rank: {game_rank}')
             with open(f'games/game_list.txt', f'{file_mode}', encoding='utf-8') as f:
                 # Move read cursor to the start of file.
                 f.seek(0)
@@ -46,9 +42,6 @@ def game_scrapper(file_mode, page_num=1):
 
     else:
         return
-
-    
-    # print(games_list)
 
 # runs the game scrapper every set number of minutes
 if __name__ == '__main__':
